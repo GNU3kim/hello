7c5414fd7360846f0d8f121b2035d6e174b48c7a
BH KIM <94www@naver.com>
2018-12-05 05:00:43 +0900

    asdasdasdasd

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ class Hello{
 		System.out.println("Hello World!");
 		System.out.println("Hello World! 2");
 		System.out.println("Hello World!asdasdasdasdasdasd 2");
+		System.out.println("asdasdasdasdasdHello World!asdasdasdasdasdasd 2");
 	}
 }

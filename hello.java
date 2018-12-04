dfc7238b62002a60db0b1011a983879b4eb3eab9
sh1453go <sh1453go@naver.com>
2018-12-05 04:56:04 +0900

    1234

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class Hello{
 	public static void main(String argv[]){
 		System.out.println("Hello World!");
 		System.out.println("Hello World!!1");
-		System.out.println("hello hello");
+		System.out.println("hello hello!");
 	}
 }
